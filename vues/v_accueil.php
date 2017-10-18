06a4d3965567d70a0410f7ba2fd8150db8935951
AnthonyBriel <anthony.briel@gmail.com>
2017-10-18 17:28:12 +0200

    t

--- vues/v_accueil.php
+++ vues/v_accueil.php
@@ -4,4 +4,3 @@
     <h1>Le domaine du verger, concours canins</h1>
     <p>Le domaine du verger est une association qui a pour but de partager la passion de nos amis les chiens. Depuis sa création en 1975 par Bill Dogue, elle effectue des compétitions sportives canines, des cours d'obéissance, et elle offre refuge aux chiens abandonnés.<br><br>Rejoignez nous !</p>
   </div>
-</div>
--- vues/v_tchat.php
+++ vues/v_tchat.php
@@ -1,1 +1,11 @@
-<div
+<div id="tchat">
+	<div id="messages">
+            <!-- les messages du tchat -->
+    </div>
+
+	<form method="POST" action="traitement.php">
+	    Pseudo : <input type="text" name="pseudo" id="pseudo" /><br />
+	    Message : <textarea name="message" id="message"></textarea><br />
+	    <input type="submit" name="submit" value="Envoyez votre message !" id="envoi" />
+	</form>
+</div>
